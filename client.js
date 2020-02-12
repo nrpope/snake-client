02e731cf8e458d7fe8f9c6ae428c81d340eb164e
Nick Pope <psydpope@gmail.com>
2020-02-12 21:31:13 +0000

    inital commit

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: "192.168.88.177",
+    port: 50541
+  });
+  conn.on("connect", () => {
+    console.log("I have connected");
+  });
+  conn.on("timeout", () => {
+    console.log("you ded cuz you idled");
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", data => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
